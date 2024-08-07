41dbb50914d8e7d90d0a78b31bc05d723fe462bd
medjs <mednajjar86@gmail.com>
2024-08-07 19:17:47 +0000

    add some class css helpers and constant data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
 
 
 function App() {
 
-
+useGSAP(()=>{
+gsap.to('#box',{
+  x: 0,
+  repeat: -1,
+  
+  
+  
+  ease:'circ',
+}
+)
+},[])
   return (
     <>
-      <p className=" text-3xl font-bold">Test</p>
+      <div id="box" className=" w-36 h-36 bg-red-600 rounded-lg m-5"></div>
     </>
   )
 }
