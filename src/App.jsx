3db279b2badd961ab214f662b6cceb5e50cff03b
medjs <mednajjar86@gmail.com>
2024-08-07 20:08:24 +0000

    add navbar hero highlights components and navbar styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
-import { useGSAP } from "@gsap/react"
-import gsap from "gsap"
+
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
 
 
 function App() {
 
-useGSAP(()=>{
-gsap.to('#box',{
-  x: 0,
-  repeat: -1,
-  
-  
-  
-  ease:'circ',
-}
-)
-},[])
   return (
-    <>
-      <div id="box" className=" w-36 h-36 bg-red-600 rounded-lg m-5"></div>
-    </>
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
   )
 }
 
