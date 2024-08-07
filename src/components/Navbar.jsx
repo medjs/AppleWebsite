96a4ee57bb3b6ba4cbc85ec694d209fe45026ae3
medjs <mednajjar86@gmail.com>
2024-08-07 21:14:07 +0000

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ import { appleImg, bagImg, searchImg } from "../utils"
 const Navbar = () => {
     return (
         <header className="w-full py-5 sm:px-5 flex justify-between items-center ">
-            <nav className="flex w-full screen-max-width">
-                <img src={appleImg} alt='Qpple image' width={14} height={18} />
+            <nav className="flex px-5 w-full screen-max-width">
+                <img src={appleImg} alt='Apple image' width={14} height={18} />
 
                 <div className="flex flex-1 justify-center max-sm:hidden">
                     {
@@ -18,7 +18,7 @@ const Navbar = () => {
                     }
                 </div>
 
-                <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+                <div className="flex items-baseline gap-7 px-5 max-sm:justify-end max-sm:flex-1">
                     <img src={searchImg} alt="search" width={18} height={18} />
                     <img src={bagImg} alt="bag" width={18} height={18} />
                 </div>
