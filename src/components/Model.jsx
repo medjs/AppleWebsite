44e987945dacc4fb833b0acc0c3d352b62ebb56b
medjs <mednajjar86@gmail.com>
2024-08-10 22:43:12 +0000

    working on three model animation

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -2,17 +2,18 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import ModelView from "./ModelView";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { yellowImg } from "../utils";
 import * as THREE from 'three';
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
 import { models, sizes } from "../constants";
+import { animateWithGsapTimeline } from "../utils/animations";
 
 gsap.registerPlugin(ScrollTrigger)
 
 const Model = () => {
- 
+
     const [size, setSize] = useState('small');
     const [model, setModel] = useState({
         title: 'iPhone 15 Pro in Natural Titanium',
@@ -27,19 +28,29 @@ const Model = () => {
     const small = useRef(new THREE.Group())
     const large = useRef(new THREE.Group())
     //rotation
-    const [smallRotatio, setSmallRotation]=useState(0)
-    const [largeRotatio, setLargeRotation]=useState(0)
+    const [smallRotation, setSmallRotation] = useState(0)
+    const [largeRotation, setLargeRotation] = useState(0)
+    const tl = gsap.timeline();
 
-    useGSAP(()=>{
+    useEffect(()=>{
+        if(size === 'large'){
+            animateWithGsapTimeline(tl, small, smallRotation, '#view1', '#view2', {transform: 'translateX(-100%)', duration:2})
+        }
+        if(size === 'small'){
+            animateWithGsapTimeline(tl, large, largeRotation, '#view2', '#view1', {transform: 'translateX(0)', duration:2})
+        }
+    },[size])
+
+    useGSAP(() => {
         gsap.to('#heading', {
-            y:0, 
-            opacity:1,
-            scrollTrigger:{trigger:"#model"},
+            y: 0,
+            opacity: 1,
+            scrollTrigger: { trigger: "#model" },
         })
-    },[])
+    }, [])
     return (
         <section className="common-padding">
-            <div  id='model' className=" screen-max-width">
+            <div id='model' className=" screen-max-width">
                 <h1 id="heading" className="section-heading">Take a closer look.</h1>
                 <div className="flex flex-col items-center mt-5">
                     <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
@@ -64,12 +75,12 @@ const Model = () => {
                         <Canvas
                             className="w-full h-full"
                             style={{
-                                position:'fixed',
+                                position: 'fixed',
                                 top: 0,
-                                bottom:0,
-                                left:0,
-                                right:0,
-                                overflow:'hidden'
+                                bottom: 0,
+                                left: 0,
+                                right: 0,
+                                overflow: 'hidden'
                             }}
                             eventSource={document.getElementById('root')}
                         >
@@ -81,19 +92,16 @@ const Model = () => {
                         <div className=" flex-center">
                             <ul className="color-container">
                                 {
-                                    models.map((item, i)=>(
-                                        <li key={i} className="w-6 h-6 rounded-full mx-2" style={{backgroundColor: item.color[0]}} onClick={()=>setModel(item)} >
+                                    models.map((item, i) => (
+                                        <li key={i} className="w-6 h-6 rounded-full mx-2 cursor-pointer" style={{ backgroundColor: item.color[0] }} onClick={() => setModel(item)} />
 
-                                        </li>
+
                                     ))
                                 }
                             </ul>
                             <button className="size-btn-container">
-                                {sizes.map(({label, value})=>(
-                                    <span key={label} className="size-btn"
-                                        style={{backgroundColor: size === value ? 'white' : 'transparent', color: size === value ? 'black' : 'white'}}
-                                        onClick={()=>setSize(value)}
-                                    >
+                                {sizes.map(({ label, value }) => (
+                                    <span key={label} className="size-btn" style={{ backgroundColor: size === value ? 'white' : 'transparent', color: size === value ? 'black' : 'white' }} onClick={() => setSize(value)}>
                                         {label}
                                     </span>
                                 ))}
